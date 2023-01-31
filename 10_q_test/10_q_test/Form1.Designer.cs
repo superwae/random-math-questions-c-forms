6d14fe67bd696c15b62ae764578a9d936bfe12a5
superwae <90796176+superwae@users.noreply.github.com>
2023-01-31 19:44:53 +0200

    Add files via upload

---
+++ 10_q_test/10_q_test/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _10_q_test
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Test));
+            this.Next_button = new System.Windows.Forms.Button();
+            this.finish_button = new System.Windows.Forms.Button();
+            this.text_answer = new System.Windows.Forms.TextBox();
+            this.label_equation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Next_button
+            // 
+            this.Next_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Next_button.Location = new System.Drawing.Point(219, 36);
+            this.Next_button.Name = "Next_button";
+            this.Next_button.Size = new System.Drawing.Size(84, 39);
+            this.Next_button.TabIndex = 1;
+            this.Next_button.Text = "Next";
+            this.Next_button.UseVisualStyleBackColor = true;
+            this.Next_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // finish_button
+            // 
+            this.finish_button.Enabled = false;
+            this.finish_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.finish_button.Location = new System.Drawing.Point(219, 110);
+            this.finish_button.Name = "finish_button";
+            this.finish_button.Size = new System.Drawing.Size(84, 39);
+            this.finish_button.TabIndex = 2;
+            this.finish_button.Text = "Finish";
+            this.finish_button.UseVisualStyleBackColor = true;
+            this.finish_button.Click += new System.EventHandler(this.finish_button_Click);
+            // 
+            // text_answer
+            // 
+            this.text_answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_answer.Location = new System.Drawing.Point(27, 118);
+            this.text_answer.Name = "text_answer";
+            this.text_answer.Size = new System.Drawing.Size(150, 31);
+            this.text_answer.TabIndex = 0;
+            this.text_answer.KeyDown += new System.Windows.Forms.KeyEventHandler(this.text_answer_KeyDown);
+            this.text_answer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.text_answer_KeyPress);
+            // 
+            // label_equation
+            // 
+            this.label_equation.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label_equation.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_equation.Location = new System.Drawing.Point(27, 36);
+            this.label_equation.Name = "label_equation";
+            this.label_equation.Size = new System.Drawing.Size(150, 39);
+            this.label_equation.TabIndex = 3;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(317, 167);
+            this.Controls.Add(this.label_equation);
+            this.Controls.Add(this.text_answer);
+            this.Controls.Add(this.finish_button);
+            this.Controls.Add(this.Next_button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Test";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Test_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Next_button;
+        private System.Windows.Forms.Button finish_button;
+        private System.Windows.Forms.TextBox text_answer;
+        private System.Windows.Forms.Label label_equation;
+    }
+}
+
---
+++ 10_q_test/10_q_test/Form1.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_q_test
+{
+    public partial class Test : Form
+    {
+        int  answer, grade=0, opr,number1,number2,page_counter=1;
+        private void text_answer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (page_counter == 11)
+            {
+                Next_button.Enabled = false;
+                finish_button.Enabled = true;   
+            }
+            else if (e.KeyChar == 13)//enter ascci code aka carriage return(cr)
+            {
+            button1_Click(sender, e);
+            }
+
+            if (/*!char.IsControl(e.KeyChar) &&*/!char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+
+        }
+
+        private void text_answer_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void finish_button_Click(object sender, EventArgs e)
+        {
+            if (text_answer.Text == answer.ToString())
+            {
+                grade++;
+            }
+            MessageBox.Show("You got " + grade.ToString() + "/10", "Final score");
+            Application.Exit();
+        }
+        char op;
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (text_answer.Text== answer.ToString())
+            {
+                grade++;
+            }
+            page_counter++;
+
+            this.Controls.Clear();
+            this.InitializeComponent();
+            
+
+            Random num1 = new Random();
+            number1 = num1.Next(2,60);
+            Random num2 = new Random();
+            number2 = num2.Next(1,number1);
+            Random oprator = new Random();
+            opr = oprator.Next(4);
+            switch (opr)
+            {
+                case 0:
+                    op = '+';
+                    answer = number1 + number2;
+                    break;
+                case 1:
+                    op = '-';
+                    answer = number1 - number2;
+                   
+                    break;
+                case 2:
+                    op = '*';
+                    
+                    if (number1 >12|| number2 > 12)
+                    {
+                        number1 = num1.Next(2,12);
+                        number2 = num2.Next(1, number1);
+                    }
+                    answer = number1 * number2;
+                    break;
+                case 3:
+                    op = '/';
+                    while (number1 % number2 != 0)
+                    {
+                        number1 = num1.Next(2,20);
+                        number2 = num2.Next(1, 100);
+                    }
+                    answer = number1 / number2;
+                    break;
+            }
+            label_equation.Text = number1.ToString() + op.ToString() + number2.ToString();
+            text_answer.Focus();
+            if (page_counter==11)
+            {
+                Next_button.Enabled = false;
+                finish_button.Enabled = true;
+            }
+        }
+        private void Test_Load(object sender, EventArgs e)
+        {
+            Next_button.Text = "Start";
+        }
+    }
+}
